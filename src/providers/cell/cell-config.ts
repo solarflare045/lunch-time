ae3037cf0e5fb0063172158688ea653ac6f2da93
Max Sorensen <max.sorensen@myriad-it.com>
2017-01-24 12:32:59 +1100

    Added Wild Fire.

--- src/providers/cell/cell-config.ts
+++ src/providers/cell/cell-config.ts
@@ -11,6 +11,7 @@ import { BaitPower } from './powers/power-bait';
 import { BombPower } from './powers/power-bomb';
 import { BuoyPower } from './powers/power-buoy';
 import { CrazyPower } from './powers/power-crazy';
+import { FirePower } from './powers/power-fire';
 import { KeyPower } from './powers/power-key';
 import { NoScopePower } from './powers/power-noscope';
 import { NuclearPower } from './powers/power-nuclear';
@@ -51,6 +52,7 @@ export const SETUP: PowerSetup[] = [
   { id: 'rev',  name: 'Reverse',      weight: 5,  power: ReversePower     , desc: 'Reverses the turn order.' },
   { id: 'saf',  name: 'Safe',         weight: 52, power: SafePower        , desc: 'No effect.' },
   { id: 'thd',  name: 'Thunder Dome', weight: 2,  power: ThunderDomePower , desc: 'You take extra turns. Next player takes a lot more.' },
+  { id: 'wfi',  name: 'Wild Fire',    weight: 2,  power: FirePower        , desc: 'Slowly burns away squares.' },
 ];
 
 export class PowerOption {
---
+++ src/providers/cell/powers/power-fire.ts
@@ -0,0 +1,43 @@
+
+import { Observable } from 'rxjs';
+import { Power } from './power';
+import _ from 'lodash';
+
+import { LosePower } from './power-lose';
+
+export class FirePower extends Power {
+  get color(): string  { return 'Wheat'; }
+  get icon(): string   { return 'bonfire'; }
+
+  action(): Observable<any> {
+    Observable.interval(3000)
+      .takeWhile(() => !this.cell.game.ended)
+      .filter(() => !this.cell.game.busy)
+      .map(() => {
+        let cell = _.sample(
+          _.chain(this.cell.game.board)
+            .filter((_cell) => !_cell.revealed)
+            .filter((_cell) => !(_cell.power instanceof LosePower || _cell.power instanceof BurnedPower))
+            .value()
+        );
+
+        if (!cell)
+          return;
+
+        let power = new BurnedPower(cell);
+        cell.setPower(power);
+        cell.setMark(power);
+      })
+      .subscribe();
+
+    return Observable.empty();
+  }
+}
+
+export class BurnedPower extends Power {
+  get color(): string { return null; }
+  get disabled(): boolean { return true; }
+  get icon(): string { return 'flame'; }
+
+  action(): Observable<any> { return Observable.empty(); }
+}
